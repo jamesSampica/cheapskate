32625114a30e3cb8a8170505c4436a49b3272954
James Sampica <jtsampica@gmail.com>
2024-03-16 08:02:19 -0500

    Update cheapskate.ps1

--- .github/workflows/cheapskate.ps1
+++ .github/workflows/cheapskate.ps1
@@ -1,1 +1,15 @@
+# Get all the resource groups
+$rgs = az group list --query "[].name" | ConvertFrom-Json
 
+# Filter out the resource groups that we don't want to delete
+$excluded = @("test1")
+$rgs = $rgs | Where-Object { $_ -NotIn $excluded }
+
+# Get stacks in each resource group and delete them
+$stacks = $rgs | ForEach-Object {
+  $rg = $_
+  az stack group list --query "[].name" -g $rg | ConvertFrom-Json | ForEach-Object {
+    Write-Host "Deleting Stack ${_} from ${rg}"
+    az stack group delete --name $_ --resource-group $rg --delete-resources --yes
+  }
+}
