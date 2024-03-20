3fb22424b89b9c88d510fda04b66371f6e87d2f8
James Sampica <jtsampica@gmail.com>
2024-03-20 07:56:13 -0500

    Update cheapskate.ps1

--- cheapskate.ps1
+++ cheapskate.ps1
@@ -4,16 +4,11 @@ $rgs = az group list --query "[].name" | ConvertFrom-Json
 # Filter out the resource groups that we don't want to delete
 $excluded = $env:EXCLUDE_STACKS ?? "[]" | ConvertFrom-Json
 
-$excluded
-
-$global:excluded = $excluded
-
 # Get stacks in each resource group and delete them
 $stacks = $rgs | ForEach-Object {
   $rg = $_
   az stack group list --query "[].name" -g $rg | ConvertFrom-Json | ForEach-Object {
-    
-    If($_ -In $global:excluded ) {
+    If($_ -In $excluded) {
       Write-Host "Skipping Stack ${_} from ${rg}"
     }
     Else {
